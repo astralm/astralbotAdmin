789540b6e77e0585e3ee5594c1ee1773e98d11cb
parshencev <parshencev.vlad@gmail.com>
2017-07-07 11:44:12 +0300

    перехват событий редусера на события входа

--- src/middleware.js
+++ src/middleware.js
@@ -5,13 +5,26 @@ export default socket => store => next => action => {
 		case types.LOGIN:
 			socket.emit(types.LOGIN, {
 				email: action.email,
-				password: action.password
+				password: action.password,
+				hash: state.getIn(['session','hash'])
 			});
 			break;
 		case types.FORGOT_PASSWORD:
 			socket.emit(types.FORGOT_PASSWORD, {
 				email: action.email
 			});
+			break;
+		case types.GET_HASH:
+			socket.emit(types.GET_HASH, {
+
+			});
+			break;
+		case types.UPDATE_USER:
+			if (action.user.hasOwnProperty('status'))
+				socket.emit(types.SET_STATUS, {
+					status: action.user.status,
+					hash: state.getIn(['session','hash'])
+				});
 			break;
 	}
 	return next(action);
