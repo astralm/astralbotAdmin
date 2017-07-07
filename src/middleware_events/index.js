aff909465efe70aa0d84c0b619bc15a4d21f6aa7
parshencev <parshencev.vlad@gmail.com>
2017-07-07 11:49:26 +0300

    Описание реакции на ws запросы сервера

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -1,13 +1,24 @@
 import * as types from '../constants/ActionTypes.js';
 import login from './events/login.js';
 import forgot_password from './events/forgot_password.js';
-
+import getHash from './events/getHash.js';
+import setHash from './events/setHash.js';
+import setStatus from './events/setStatus.js';
 const initEventListeners = (socket, store) => {
 	socket.on(types.LOGIN, data => {
 		login(store, data);
 	});
 	socket.on(types.FORGOT_PASSWORD, data => {
 		forgot_password(store, data);
+	});
+	socket.on(types.CONNECT, data => {
+		getHash(store, data);
+	});
+	socket.on(types.SET_HASH, data => {
+		setHash(store, data);
+	});
+	socket.on(types.DISCONNECT, data => {
+		setStatus(store, "offline");
 	});
 }
 
