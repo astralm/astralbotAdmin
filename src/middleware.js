435b72e4603672689797029aa5013a417d87bce6
parshencev <parshencev.vlad@gmail.com>
2017-07-10 12:08:27 +0300

    изменение в перехвате событий редусера

--- src/middleware.js
+++ src/middleware.js
@@ -2,30 +2,21 @@ import * as types from 'constants/ActionTypes.js';
 export default socket => store => next => action => {
 	const state = store.getState().app
 	switch (action.type){
-		case types.LOGIN:
+		case types.LOGIN : {
 			socket.emit(types.LOGIN, {
+				hash: state.getIn(['session', 'hash']),
 				email: action.email,
-				password: action.password,
-				hash: state.getIn(['session','hash'])
+				password: action.password
 			});
 			break;
-		case types.FORGOT_PASSWORD:
-			socket.emit(types.FORGOT_PASSWORD, {
-				email: action.email
+		}
+		case types.LOGOUT : {
+			socket.emit(types.SET_STATUS, {
+				status_id: 2,
+				hash: state.getIn(['session', 'hash'])	
 			});
 			break;
-		case types.GET_HASH:
-			socket.emit(types.GET_HASH, {
-
-			});
-			break;
-		case types.UPDATE_USER:
-			if (action.user.hasOwnProperty('status'))
-				socket.emit(types.SET_STATUS, {
-					status: action.user.status,
-					hash: state.getIn(['session','hash'])
-				});
-			break;
+		}
 	}
 	return next(action);
 }
