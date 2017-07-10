4d9d2d38875434584c40a4a3e31d1ba6c3dca0ca
parshencev <parshencev.vlad@gmail.com>
2017-07-10 12:30:21 +0300

    изменение в редусере

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -7,17 +7,23 @@ const AppReducer = (state = Map(), action) => {
 			return state.merge(fromJS(typeof action.state == "string" ? 
 				JSON.parse(action.state) : 
 				action.state));
-		case types.LOGOUT :
-			return state.set('user', state.get('user') ? 
-				state.get('user').set('status', false) : 
-				fromJS({status: false}));
 		case types.UPDATE_USER :
 			return action.user ? 
 				state.set('user', state.get('user') ? 
 					state.get('user').merge(fromJS(action.user)) :
 					fromJS(action.user)
 				) : state;
-		case types.SET_HASH :
+		case types.STATUS : 
+			return action.status ? 
+				state.set('user', state.get('user') ?
+					state.get('user').merge(fromJS({status: "online"})) :
+					fromJS({status: "online"})
+				) : state.set('user', state.get('user') ?
+					state.get('user').merge(fromJS({status: "offline"})) :
+					fromJS({status: "offline"})
+				);
+		case types.HASH :
+			console.log(action.hash);
 			return action.hash ?
 				state.set('session', state.get('session') ?
 					state.get('session').merge(fromJS({hash: action.hash})) :
