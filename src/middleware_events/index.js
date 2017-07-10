44b7d88f0a2f0f9eac618f54ec7daff8c1de7df7
parshencev <parshencev.vlad@gmail.com>
2017-07-10 12:29:53 +0300

    переключатель событий ws

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -1,24 +1,19 @@
 import * as types from '../constants/ActionTypes.js';
-import login from './events/login.js';
-import forgot_password from './events/forgot_password.js';
-import getHash from './events/getHash.js';
-import setHash from './events/setHash.js';
-import setStatus from './events/setStatus.js';
+import Hash from './events/HASH.js';
+import Login from './events/LOGIN.js';
+import SetStatus from './events/SET_STATUS.js';
 const initEventListeners = (socket, store) => {
+	socket.on(types.HASH, data => {
+		Hash(store, data);
+	});
 	socket.on(types.LOGIN, data => {
-		login(store, data);
+		Login(store, data);
 	});
-	socket.on(types.FORGOT_PASSWORD, data => {
-		forgot_password(store, data);
+	socket.on(types.SET_STATUS, data => {
+		SetStatus(store, data);
 	});
-	socket.on(types.CONNECT, data => {
-		getHash(store, data);
-	});
-	socket.on(types.SET_HASH, data => {
-		setHash(store, data);
-	});
-	socket.on(types.DISCONNECT, data => {
-		setStatus(store, "offline");
+	socket.on('disconnect', () => {
+		SetStatus(store, 2);
 	});
 }
 
