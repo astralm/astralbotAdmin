304f3a846390646622a278ac7a3898c659cf2a95
parshencev <parshencev.vlad@gmail.com>
2017-07-11 12:27:46 +0300

    модернизация события получения ключа сесии

--- src/middleware_events/events/HASH.js
+++ src/middleware_events/events/HASH.js
@@ -1,4 +1,6 @@
-import { Hash } from '../../actions/index.js';
+import { Hash, login } from '../../actions/index.js';
 export default (store, data) => {
 	store.dispatch(Hash(data));
+	var state = store.getState().app;
+	store.dispatch(login(state.getIn(['user', 'email']), state.getIn(['user', 'password'])));
 }
