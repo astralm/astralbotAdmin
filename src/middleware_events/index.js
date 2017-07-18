ea7402c9546958a177700fe624633fff18c0af2d
parshencev <parshencev.vlad@gmail.com>
2017-07-18 13:53:40 +0300

    описание реакции на вызов событий выхода с сервера

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -2,5 +2,6 @@ import * as Types from '../constants/ActionTypes.js';
 import setStatus from './events/SET_STATUS.js';
 const initEventListeners = (socket, store) => {
 	socket.on(Types.LOGIN, setStatus(store));
+	socket.on(Types.LOGOUT, setStatus(store));
 }
 module.exports = initEventListeners;
