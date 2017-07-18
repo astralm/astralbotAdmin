0e72ffa96165fea9b2f34e9df193f9508a077154
parshencev <parshencev.vlad@gmail.com>
2017-07-18 11:23:22 +0300

    добавление перехватчика событий

--- src/middleware.js
+++ src/middleware.js
@@ -2,21 +2,12 @@ import * as types from 'constants/ActionTypes.js';
 export default socket => store => next => action => {
 	const state = store.getState().app
 	switch (action.type){
-		case types.LOGIN : {
+		case types.LOGIN :
 			socket.emit(types.LOGIN, {
-				hash: state.getIn(['session', 'hash']),
 				email: action.email,
 				password: action.password
 			});
 			break;
-		}
-		case types.LOGOUT : {
-			socket.emit(types.SET_STATUS, {
-				status_id: 2,
-				hash: state.getIn(['session', 'hash'])	
-			});
-			break;
-		}
 	}
 	return next(action);
 }
