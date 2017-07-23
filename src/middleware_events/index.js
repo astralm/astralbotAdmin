d0d908481375718b79ae6f97b4f6e5f73e1a21b0
parshencev <parshencev.vlad@gmail.com>
2017-07-23 13:47:03 +0300

    добавление реакции на вызов обновления информации пользователя с сервера

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -1,9 +1,11 @@
 import * as Types from '../constants/ActionTypes.js';
 import setStatus from './events/SET_STATUS.js';
 import setUsers from './events/SET_USERS.js';
+import updateUser from './events/UPDATE_USER.js';
 const initEventListeners = (socket, store) => {
 	socket.on(Types.LOGIN, setStatus(store));
 	socket.on(Types.LOGOUT, setStatus(store));
 	socket.on(Types.GET_USERS, setUsers(store));
+	socket.on(Types.UPDATE_USER, updateUser(store));
 }
 module.exports = initEventListeners;
