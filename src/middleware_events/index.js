25cd3ae10ebf6417fd399d20ea81e022a230e191
parshencev <parshencev.vlad@gmail.com>
2017-07-24 15:10:44 +0300

    реакция на вызов сервером функции получения сессий

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -1,9 +1,13 @@
 import * as Types from '../constants/ActionTypes.js';
 import setStatus from './events/SET_STATUS.js';
 import setUsers from './events/SET_USERS.js';
+import updateUser from './events/UPDATE_USER.js';
+import setSessions from './events/SET_SESSIONS.js';
 const initEventListeners = (socket, store) => {
 	socket.on(Types.LOGIN, setStatus(store));
 	socket.on(Types.LOGOUT, setStatus(store));
 	socket.on(Types.GET_USERS, setUsers(store));
+	socket.on(Types.UPDATE_USER, updateUser(store));
+	socket.on(Types.GET_SESSIONS, setSessions(store));
 }
 module.exports = initEventListeners;
