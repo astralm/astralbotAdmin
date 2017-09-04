ef52571e1cbecbeffb00ab3affbc3ebfc7488aa4
parshencev <parshencev.vlad@gmail.com>
2017-09-04 20:35:56 +0300

    удаление ненужного пути

--- src/client.js
+++ src/client.js
@@ -50,11 +50,9 @@ const rootRoute = {
     childRoutes: [
       require('./routes/app'),
       require('./routes/404'),
-      require('./routes/500'),
       require('./routes/confirmEmail'),
       require('./routes/forgotPassword'),
       require('./routes/login'),
-
       {
         path: '*',
         indexRoute: { onEnter: (nextState, replace) => replace('/404') },
