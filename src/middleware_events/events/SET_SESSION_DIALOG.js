c9698fe48e5bbca4813e15fc22541c97cf89486f
parshencev <parshencev.vlad@gmail.com>
2017-09-05 17:57:02 +0300

    удаление лишних уведомлений

--- src/middleware_events/events/SET_SESSION_DIALOG.js
+++ src/middleware_events/events/SET_SESSION_DIALOG.js
@@ -1,20 +1,4 @@
 import * as events from '../../actions/index.js';
 export default store => data => {
-	let state = store.getState().app;
-	if(state.get('notification')){
-		let notification = Notification || window.Notification;
-		if(notification.permission == "granted"){
-			let size = state.getIn(['session', 'dialog']) ? state.getIn(['session', 'dialog']).size : 0;
-			if(size != data.length && !data[data.length - 1].answer_message){
-				let message = new notification("Новое сообщение в открытом диалоге: ", {
-					body: data[data.length - 1].question_message,
-					requireInteraction: true
-				});
-				message.onclick = () => {
-					window.alert("Новое сообщение в открытом диалоге: " + data[data.length - 1].question_message);
-				}
-			}
-		}
-	}
 	store.dispatch(events.setSessionDialog(data));
 }
