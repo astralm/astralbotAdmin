11f91c98964854029751f0c5a64002854543b34e
parshencev <parshencev.vlad@gmail.com>
2017-09-20 18:09:53 +0300

    изменение в событии обновления пользовательской информации

--- src/middleware_events/events/UPDATE_USER.js
+++ src/middleware_events/events/UPDATE_USER.js
@@ -3,8 +3,5 @@ export default store => data => {
 	if(data && data[0]){
 		data = data[0];
 		store.dispatch(actions.updateUser(data.user_email, data.user_password, data.user_name, data.user_status, data.user_id));
-		store.dispatch(actions.validate(true));
-	} else {
-		store.dispatch(actions.validate(false));
 	}
 }
