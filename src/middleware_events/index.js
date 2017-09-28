ab8d28927efc01ca0fe24ca9a9f05c8ad2823b70
parshencev <parshencev.vlad@gmail.com>
2017-09-28 18:08:24 +0300

    роутинг событий с сервера

--- src/middleware_events/index.js
+++ src/middleware_events/index.js
@@ -7,6 +7,7 @@ import setSessionInfo from './events/SET_SESSION_INFO.js';
 import setSessionDialog from './events/SET_SESSION_DIALOG.js';
 import setBotStatus from './events/SET_BOT_STATUS.js';
 import sendEmail from './events/SEND_EMAIL.js';
+import setDispatches from './events/SET_DISPATCHES.js';
 const initEventListeners = (socket, store) => {
 	socket.on(Types.LOGIN, setStatus(store));
 	socket.on(Types.LOGOUT, setStatus(store));
@@ -17,5 +18,6 @@ const initEventListeners = (socket, store) => {
 	socket.on(Types.GET_SESSION_DIALOG, setSessionDialog(store));
 	socket.on(Types.GET_BOT_STATUS, setBotStatus(store));
 	socket.on(Types.SEND_EMAIL, sendEmail(store));
+	socket.on(Types.GET_DISPATCHES, setDispatches(store));
 }
 module.exports = initEventListeners;
