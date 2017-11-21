e9d6ddde34cc1c54c45db4d466f625346cd5acb3
parshencev <parshencev.vlad@gmail.com>
2017-11-21 17:40:57 +0300

    модернизация получения информации о сессии: зависимость от организации

--- src/middleware_events/events/SET_SESSION_INFO.js
+++ src/middleware_events/events/SET_SESSION_INFO.js
@@ -1,4 +1,9 @@
 import * as actions from '../../actions/index.js';
 export default store => data => {
-	store.dispatch(actions.setSessionInfo(data));
+	var state = store.getState().app;
+	if(state.getIn(['user', 'organization_id']) == data[0].organization_id){
+		store.dispatch(actions.setSessionInfo(data));
+	} else {
+		store.dispatch(actions.setSessionInfo([]));
+	}
 }
